19f030741b32f6d166f94d80753cd94f9b7ea5dd
Yuri Z <yuri.zalvovsky@gmail.com>
2017-08-09 14:11:19 +0300

    Migration to Spike is done.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const Contentful = require('spike-contentful')
+
 const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
 const jsStandards = require('spike-js-standards')
@@ -7,7 +9,6 @@ const sugarss = require('sugarss')
 const env = process.env.NODE_ENV
 
 const slugify = require('slugify')
-const Contentful = require('spike-contentful')
 const locals = {}
 
 
@@ -18,18 +19,23 @@ module.exports = {
       addDataTo: locals,
       accessToken: 'eb6d896c25cd46f3fa4a4f72a4307fdfaf63cca840ad7624d8218054d5310b33',
       spaceId: 'qyijm2cbwnd5',
+      // locale: '*', // MY
       contentTypes: [
         {
           name: 'products',
           id: 'product',
           template: {
             path: 'views/product.sgr',
-            output: (e) => { return `products/${e.id}.html` }
+            output: (e) => { return `products/${e.fields.productUrl}.html` }
           }
         },
         {
           name: 'textPages',
           id: 'textPage',
+          template: {
+            path: 'views/textpage.sgr',
+            output: (e) => { return `${e.fields.footerUrl}.html` }
+          }
         },
         {
           name: 'contactPage',
@@ -50,7 +56,7 @@ module.exports = {
     parser: sugarml,
     // locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } }, // original spike template ver.
     // locals: () => locals, // spike-contentful ver.
-    locals: (ctx) => Object.assign({ pageId: pageId(ctx), slugme: slugify }, locals), // gitter fix ver.
+    locals: (ctx) => Object.assign({ pageId: pageId(ctx), slugme: slugify, foo: 'bar' }, locals), // gitter fix ver.
     minify: env === 'production'
   }),
   postcss: cssStandards({
