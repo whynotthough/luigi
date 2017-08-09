b69f65bf88c3b1cae1b40c7c3235332d56a6217e
Yuri Z <yuri.zalvovsky@gmail.com>
2017-08-09 18:55:27 +0300

    IT language

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,29 @@
-console.log('you can use ES6 here : )')
+// console.log('you can use ES6 here : )')
+
+// if (window.location.pathname === '/') {
+// 	location.assign(newPath)
+// }
+
+
+// LANGUAGE HANDLER
+var langOptions = document.querySelectorAll('.lang-option')
+langOptions.forEach(function(lo){
+  lo.addEventListener('click', function(){
+    var clickedLang = lo.innerHTML.toLowerCase(),
+        currPath = window.location.pathname,
+        newPath
+
+    if (lo.classList.value.indexOf('active') < 0){
+      if (clickedLang === 'en'){
+        newPath = currPath.slice(3,currPath.length)
+        location.assign(newPath)
+        // console.log(newPath)
+      } else if (clickedLang === 'it') {
+        newPath = '/' + clickedLang.concat(currPath)
+        location.assign(newPath)
+        // console.log(newPath.concat(currPath))
+      }
+    }
+
+  })
+})
