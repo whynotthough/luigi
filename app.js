c08f4bb410bf78ae4d750ec4be944c08760ba8d1
Yuri Z <yuri.zalvovsky@gmail.com>
2017-11-18 01:39:26 +0300

    Temp delete "netlify" for the email form

--- app.js
+++ app.js
@@ -18,13 +18,14 @@ module.exports = {
       addDataTo: locals,
       accessToken: 'eb6d896c25cd46f3fa4a4f72a4307fdfaf63cca840ad7624d8218054d5310b33',
       spaceId: 'qyijm2cbwnd5',
+      // locale: '*', // MY
       contentTypes: [
         {
           name: 'products',
           id: 'product',
           template: {
             path: 'views/product.sgr',
-            output: (e) => `products/${e.fields.productUrl}.html`
+            output: (e) => { return `products/${e.fields.productUrl}.html` }
           }
         },
         {
@@ -32,7 +33,7 @@ module.exports = {
           id: 'product',
           template: {
             path: 'views/it/product.sgr',
-            output: (e) => `it/products/${e.fields.productUrl}.html`
+            output: (e) => { return `it/products/${e.fields.productUrl}.html` }
           }
         },
         {
@@ -40,7 +41,7 @@ module.exports = {
           id: 'textPage',
           template: {
             path: 'views/textpage.sgr',
-            output: (e) => `${e.fields.footerUrl}.html`
+            output: (e) => { return `${e.fields.footerUrl}.html` }
           }
         },
         {
@@ -48,7 +49,7 @@ module.exports = {
           id: 'textPage',
           template: {
             path: 'views/it/textpage.sgr',
-            output: (e) => `it/${e.fields.footerUrl}.html`
+            output: (e) => { return `it/${e.fields.footerUrl}.html` }
           }
         },
         {
@@ -68,6 +69,9 @@ module.exports = {
   ignore: ['**/_*/*', '**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock', 'package-lock.json'],
   reshape: htmlStandards({
     parser: sugarml,
+    // locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } }, // original spike template ver.
+    // locals: () => locals, // spike-contentful ver.
+    // locals: (ctx) => Object.assign({ pageId: pageId(ctx), slugme: slugify, foo: 'bar' }, locals), // gitter fix ver.
     locals: (ctx) => Object.assign(locals, { pageId: pageId(ctx), slugme: slugify, foo: 'bar' }), // gitter fix ver.
     minify: env === 'production'
   }),
